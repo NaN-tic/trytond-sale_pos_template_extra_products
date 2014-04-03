77aaa943337001671412c8514f08f7df57c9c9e0
?ngel ?lvarez <angel@nan-tic.com>
2014-04-03 10:22:16 +0200

    Fix copy

--- sale.py
+++ sale.py
@@ -100,6 +100,8 @@ class SaleLine:
         default['template_extra_childs'] = None
         new_lines = super(SaleLine, cls).copy(lines, default=default)
 
+        lines = sorted(lines, key=lambda a: (a.template, a.product))
+        new_lines = sorted(new_lines, key=lambda a: (a.template, a.product))
         new_line_by_line = dict((l, nl) for l, nl in zip(lines, new_lines))
         for new_line in new_lines:
             parent_line = new_line.template_extra_parent
