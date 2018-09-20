84a95ea9a90dd99803b096d51ea0a66e7ec8ce85
Carlos G?lvez <carlos.galvez@nan-tic.com>
2018-09-20 11:45:15 +0200

    Fix the inheritance of the on_change_party method of the Sale model.

--- sale.py
+++ sale.py
@@ -66,10 +66,14 @@ class Sale:
             },
         depends=['state'])
 
-    @fields.depends('party', 'extra_services')
+    @classmethod
+    def __setup__(cls):
+        super(Sale, cls).__setup__()
+        if 'extra_services' not in cls.party.on_change:
+            cls.party.on_change.add('extra_services')
+
     def on_change_party(self):
         super(Sale, self).on_change_party()
-
         if self.extra_services:
             self.extra_services = None
         if self.party and self.party.default_extra_services:
