f3d811541f22549cbaa07c647dc79515114dd88d
Albert Cervera i Areny <albert@nan-tic.com>
2020-12-05 00:47:19 +0100

    Pyflakes fixes.

--- __init__.py
+++ __init__.py
@@ -1,19 +1,19 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .sale import *
+from . import sale
 
 
 def register():
     Pool.register(
-        Template,
-        Party,
-        PartyExtraProduct,
-        Sale,
-        SaleExtraProduct,
-        SaleLine,
-        SetQuantitiesStart,
+        sale.Template,
+        sale.Party,
+        sale.PartyExtraProduct,
+        sale.Sale,
+        sale.SaleExtraProduct,
+        sale.SaleLine,
+        sale.SetQuantitiesStart,
         module='sale_pos_template_extra_products', type_='model')
     Pool.register(
-        SetQuantities,
+        sale.SetQuantities,
         module='sale_pos_template_extra_products', type_='wizard')
